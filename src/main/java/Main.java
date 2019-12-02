8002a4c218e69ecfefdf2ec1ee8e901448858a7d
SandyPham823 <sandydupham@gmail.com>
2019-12-02 16:01:12 -0500

    starting fresh, deleted Levenshtein

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main extends JerkSONParser {
 
@@ -7,6 +14,19 @@ public class Main extends JerkSONParser {
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
+    }
+
+    public ArrayList readRawDataToArray() throws Exception{
+        BufferedReader bufReader = new BufferedReader(new FileReader("RawData.txt"));
+        ArrayList<String> listOfLines = new ArrayList<>();
+
+        String line = bufReader.readLine();
+        while (line != null) {
+            listOfLines.add(line);
+            line = bufReader.readLine();
+        }
+        bufReader.close();
+        return listOfLines;
     }
 
     public static void main(String[] args) throws Exception{
